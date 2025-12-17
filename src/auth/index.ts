61d2b88b9155bab705ca3360c7a4b85da2c7069e
Marco <marco@brevitydata.net>
2025-12-17 14:16:57 +0800

    fix: correct import path for types in auth module

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,5 +1,5 @@
 export * from "./config";
-export * from "./types";
+export * from "./types.d";
 export * from "./tokenStore";
 export * from "./api/login";
 export * from "./api/refreshAccessToken";
